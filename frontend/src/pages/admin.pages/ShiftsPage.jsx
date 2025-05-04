866e3978ecb26e3784b3244db34b1533c1970c23
Bublaaa <yanuarkrisna7@gmail.com>
2025-05-04 13:33:12 +0700

    Add reusable modal and delete form

--- frontend/src/pages/admin.pages/ShiftsPage.jsx
+++ frontend/src/pages/admin.pages/ShiftsPage.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useShiftStore } from "../../../store/shiftStore.js";
 import Button from "../../components/Button.jsx";
 import { Trash2, PenBoxIcon, Plus } from "lucide-react";
@@ -8,6 +8,9 @@ import { motion } from "framer-motion";
 import dayjs from "dayjs";
 import { NavLink } from "react-router-dom";
 import { formatTime } from "../../utils/dateFormatter.js";
+import Modal from "../../components/Modal.jsx";
+import { DeleteConfirmationForm } from "../../components/DeleteConfirmationForm.jsx";
+import { useNavigate } from "react-router-dom";
 
 const ShiftProgressBar = ({ startTime, endTime }) => {
   const start = dayjs(startTime);
@@ -50,10 +53,39 @@ const ShiftProgressBar = ({ startTime, endTime }) => {
 };
 
 const ShiftsPage = ({}) => {
-  const { shifts, fetchShifts, isLoading } = useShiftStore();
+  const navigate = useNavigate();
+  const { shifts, fetchShifts, isLoading, deleteShift } = useShiftStore();
   useEffect(() => {
     fetchShifts();
   }, []);
+
+  const [modalState, setModalState] = useState({
+    isOpen: false,
+    title: "",
+    body: null,
+  });
+  const openModal = (title, body) =>
+    setModalState({ isOpen: true, title, body });
+  const closeModal = () =>
+    setModalState({ isOpen: false, title: "", body: null });
+
+  const handleDeleteAction = (e) => {
+    const deleteButton = e.target.closest(".delete-btn");
+    if (deleteButton) {
+      openModal(
+        "Delete Shift",
+        <DeleteConfirmationForm
+          itemName={deleteButton.dataset.name}
+          onDelete={deleteShift}
+          itemId={deleteButton.dataset.id}
+          onClose={closeModal}
+          // redirect={navigate(-1)}
+        />
+      );
+      return;
+    }
+  };
+
   if (isLoading) {
     return <LoadingSpinner />;
   }
@@ -72,38 +104,53 @@ const ShiftsPage = ({}) => {
           </Button>
         </NavLink>
       </motion.div>
-      {shifts.map((shift, index) => {
-        return (
-          <motion.div
-            key={shift._id}
-            initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.25 * (index + 1) }}
-            className="grid grid-cols-4 items-center p-3 rounded-lg bg-white hover:bg-gray-100"
-          >
-            <div>{toTitleCase(shift.name)}</div>
-            <div className="col-span-2 flex flex-col gap-2 items-center">
-              <p>
-                {formatTime(shift.startTime)}-{formatTime(shift.endTime)}
-              </p>
-              <ShiftProgressBar
-                startTime={shift.startTime}
-                endTime={shift.endTime}
-              />
-            </div>
-            <div className="flex flex-row justify-end gap-2">
-              <NavLink to={`/admin/shifts/${shift._id}`}>
-                <Button
-                  buttonType="secondary"
-                  buttonSize="icon"
-                  icon={PenBoxIcon}
+      <Modal
+        isOpen={modalState.isOpen}
+        onClose={closeModal}
+        title={modalState.title}
+        body={modalState.body}
+      />
+      <div onClick={(e) => handleDeleteAction(e)}>
+        {shifts.map((shift, index) => {
+          return (
+            <motion.div
+              key={shift._id}
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.25 * (index + 1) }}
+              className="grid grid-cols-4 items-center p-3 rounded-lg bg-white hover:bg-gray-100"
+            >
+              <div>{toTitleCase(shift.name)}</div>
+              <div className="col-span-2 flex flex-col gap-2 items-center">
+                <p>
+                  {formatTime(shift.startTime)}-{formatTime(shift.endTime)}
+                </p>
+                <ShiftProgressBar
+                  startTime={shift.startTime}
+                  endTime={shift.endTime}
                 />
-              </NavLink>
-              <Button buttonType="danger" buttonSize="icon" icon={Trash2} />
-            </div>
-          </motion.div>
-        );
-      })}
+              </div>
+              <div className="flex flex-row justify-end gap-2">
+                <NavLink to={`/admin/shifts/${shift._id}`}>
+                  <Button
+                    buttonType="secondary"
+                    buttonSize="icon"
+                    icon={PenBoxIcon}
+                  />
+                </NavLink>
+                <Button
+                  className="delete-btn"
+                  buttonType="danger"
+                  buttonSize="icon"
+                  icon={Trash2}
+                  data-id={shift._id}
+                  data-name={shift.name}
+                />
+              </div>
+            </motion.div>
+          );
+        })}
+      </div>
     </div>
   );
 };
